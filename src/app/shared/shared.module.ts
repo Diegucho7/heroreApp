fd100a2da9dce8b9adc0f3d151259b38145c9b89
Diegucho7 <diegucho7t@gmail.com>
2023-08-21 23:51:40 -0500

    continuacion de rutas hijas

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { Error404PageComponent } from './pages/error404-page/error404-page.component';
+
+
+@NgModule({
+
+    exports: [Error404PageComponent],
+    declarations: [ Error404PageComponent ],
+   
+})
+export class SharedModule { }
