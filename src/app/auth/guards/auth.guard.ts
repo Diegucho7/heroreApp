631e68ef35a6c68cc465442749995b8508a7f25c
Diego <dvelarde140@gmail.com>
2023-10-13 16:53:49 -0500

    solucionado el auth - con new version

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,59 +1,56 @@
-import { Injectable } from '@angular/core';
+import {  inject } from '@angular/core';
 import {
     ActivatedRouteSnapshot,
     CanActivateFn,
     CanMatchFn,
     Route,
+    Router,
     RouterStateSnapshot,
     UrlSegment,
-  } from '@angular/router';import { Observable, tap } from 'rxjs';
+  } from '@angular/router';
+  import { Observable, tap } from 'rxjs';
 
 import { AuthService } from '../services/auth.service';
 
-@Injectable({ providedIn: 'root' })
-export class AuthGuard {
 
 
-  constructor(
-    private authService: AuthService,
-    
-  ) { }
-
-  private checkAuthStatus(): boolean | Observable<boolean> {
-
-    return this.authService.checkAuthentication()
-      .pipe(
-        tap( isAuthenticated => console.log('Authenticated:', isAuthenticated ) ),
-        tap( isAuthenticated => {
-          if ( !isAuthenticated ) {
-          //  this.router.navigate(['./auth/login'])
-          }
-        }),
-
-      )
-
-  }
+const checkAuthStatus = (): boolean | Observable<boolean> => {
+    //se inyectan el AuthService y el Router
+    const authService: AuthService = inject(AuthService);
+    const router: Router = inject(Router);
+   
+    return authService.checkAuthentication().pipe(
+      tap((isAuthenticated) => {
+        if (!isAuthenticated) {
+          router.navigate(['/auth/login']);
+        }
+      })
+    );
+  };
 
 
- 
+export const canActivateGuard: CanActivateFn = (
+    //Hay que tener en cuenta el tipado CanActiveFn
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ) => {
+    console.log('CanActivate');
+    console.log({ route, state });
+   
+    return checkAuthStatus();
+  };
+   
+  export const canMatchGuard: CanMatchFn = (
+    //Tipado CanMatchFN
+    route: Route,
+    segments: UrlSegment[]
+  ) => {
+    console.log('CanMatch');
+    console.log({ route, segments });
+   
+    return checkAuthStatus();
+  };
 
-}
-export const canActivateGuard: CanActivateFn = ( //Hay que tener en cuenta el tipado CanActiveFn
-  route: ActivatedRouteSnapshot,
-  state: RouterStateSnapshot
-) => {
-  console.log('CanActivate');
-  console.log({ route, state });
- 
-  return true;
-};
- 
-export const canMatchGuard: CanMatchFn = ( //Tipado CanMatchFN
-  route: Route,
-  segments: UrlSegment[]
-) => {
-  console.log('CanMatch');
-  console.log({ route, segments });
- 
-  return true;
-};
+//function checkAuthStatus(): boolean | import("@angular/router").UrlTree | Observable<boolean | import("@angular/router").UrlTree> | Promise<boolean | import("@angular/router").UrlTree> {
+  //  throw new Error('Function not implemented.');
+//}
